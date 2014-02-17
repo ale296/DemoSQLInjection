4c7050d0423252bf57af4ee6d639f672b96ddf39
krille0x7c2 <bodelsson@gmail.com>
2014-02-17 20:28:42 +0100

    Added interface

--- src/demosqlinject/MainFrame.java
+++ src/demosqlinject/MainFrame.java
@@ -13,7 +13,7 @@ import javax.swing.JOptionPane;
  */
 public class MainFrame extends javax.swing.JFrame {
 
-    DatabaseInterface db = new Database("userdatabase", "mako", "whatevs");//Change password and username to your mysql preferences!!! 
+    DatabaseInterface db = new Database("userdatabase", "username", "password");//Change password and username to your mysql preferences!!! 
 
     public MainFrame() {
         initComponents();
