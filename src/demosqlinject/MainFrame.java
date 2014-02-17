d0ca6659b6074db6e663b7016bc2e280be1b817c
krille0x7c2 <bodelsson@gmail.com>
2014-02-17 16:09:44 +0100

    Added driver

--- src/demosqlinject/MainFrame.java
+++ src/demosqlinject/MainFrame.java
@@ -13,7 +13,7 @@ import javax.swing.JOptionPane;
  */
 public class MainFrame extends javax.swing.JFrame {
 
-    Database db = new Database("userdatabase", "username", "password");//Change password and username to your mysql preferences!!!
+    Database db = new Database("userdata", "username", "password");//Change password and username to your mysql preferences!!! 
 
     public MainFrame() {
         initComponents();
